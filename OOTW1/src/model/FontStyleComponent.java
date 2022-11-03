da77193b945b0f0a785ff552429029558d69a9d5
Aven910126 <a1148370488@gmail.com>
2022-11-03 21:59:43 +0800

    Decorator_finall2

--- OOTW1/src/model/FontStyleComponent.java
+++ OOTW1/src/model/FontStyleComponent.java
@@ -2,9 +2,9 @@ package model;
 import javax.swing.text.StyledDocument;
 
 // Componet
-public abstract class FontStyleComponent {
-    abstract void  setStyle(StyledDocument styledDocument, int startLocation, int endLocation);
-    abstract public  FontStyleComponent removeDecorator(FontStyleComponent toRemove);
-    abstract protected FontStyleComponent getSubject();
+interface FontStyleComponent {
+     void  setStyle(StyledDocument styledDocument, int startLocation, int endLocation);
+     FontStyleComponent removeDecorator(FontStyleComponent toRemove);
+     FontStyleComponent getSubject();
 
 }

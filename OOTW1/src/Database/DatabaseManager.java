eb30ae085e7b51b429783aec866cf26758b82795
fan9704 <cxz123499@gmail.com>
2022-11-12 23:47:53 +0800

    Fix OODB Manager with Repository

--- OOTW1/src/Database/DatabaseManager.java
+++ OOTW1/src/Database/DatabaseManager.java
@@ -14,8 +14,9 @@ public class DatabaseManager {
         documentCollection = new ArrayDocumentCollection();
     }
 
-    void fetchModel() {
+    private void fetchModel() {
         documentCollection.removeAll();
+        DatabaseRepository dbRepository = new DatabaseRepository();
         List<DocumentModel> documentModelList = dbRepository.fetchDbDocumentModelList();
         for (DocumentModel documentModel : documentModelList) {
             documentCollection.add(documentModel);
@@ -23,20 +24,28 @@ public class DatabaseManager {
     }
 
     public void createDocumentModel(DocumentModel documentModel) {
+        DatabaseRepository dbRepository = new DatabaseRepository();
         dbRepository.createDocumentModel(documentModel);
         documentCollection.add(documentModel);
     }
 
     public void updateDocumentModel(DocumentModel documentModel) {
+        DatabaseRepository dbRepository = new DatabaseRepository();
         dbRepository.updateDbDocument(documentModel);
     }
 
     public void deleteDocumentModel(DocumentModel documentModel) {
-        dbRepository.deleteDbDocument(documentModel);
+        DatabaseRepository dbRepository = new DatabaseRepository();
+        dbRepository.deleteDbDocument(documentModel.documentId);
     }
 
     public DocumentIterator getDocumentVersionIterator(String order) {
         fetchModel();
         return documentCollection.getIterator(order);
+    }
+
+    static public void closeConnection(){
+        DBConnector dbConnector = DBConnector.getInstance();
+        dbConnector.closeConnection();
     }
 }
